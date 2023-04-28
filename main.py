682c972f1327d427bda9cb00184345b8ad757f78
Carson740 <61304405+Carson740@users.noreply.github.com>
2023-04-28 16:16:23 -0500

    Improved design. Added support for Generic Printer

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+# main.py
+
 from flask import Flask, render_template, request, redirect, url_for
 import subprocess
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     printer_ip = ''
     if request.method == 'POST':
         printer_ip = request.form['printer_ip']
-        # Run the script with the specified printer IP address
-        subprocess.run(['python', 'printTo.py', printer_ip])
+        if 'print_pdf' in request.form:
+            # Run the script with the specified printer IP address and command 'print_pdf'
+            print(printer_ip, "print_pdf")
+            subprocess.run(['python', 'printTo.py', 'print_pdf', printer_ip])
+        elif 'print_4x6label' in request.form:
+            # Run the script with the specified printer IP address and command 'print_text'
+            print(printer_ip, "print_4x6label")
+            subprocess.run(['python', 'printTo.py', 'print_4x6label', printer_ip])
         return redirect(url_for('home', printer_ip=printer_ip))
     else:
         if 'printer_ip' in request.args:
